54e562a2e0b559e447b81a9c713bd5c36f324859
100gammmit <100minha@naver.com>
2025-03-26 16:36:25 +0900

    feat: id 필드 생성

--- src/main/java/com/ddobang/backend/domain/store/entity/Store.java
+++ src/main/java/com/ddobang/backend/domain/store/entity/Store.java
@@ -1,11 +1,20 @@
 package com.ddobang.backend.domain.store.entity;
 
 import com.ddobang.backend.global.entity.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store extends BaseTime {
-    @Id
-    Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
 }
