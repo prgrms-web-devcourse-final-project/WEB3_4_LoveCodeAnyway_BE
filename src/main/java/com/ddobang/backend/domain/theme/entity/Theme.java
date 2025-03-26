2cc62135f721dbda0af6707939a1199887a928a1
100gammmit <100minha@naver.com>
2025-03-26 16:13:19 +0900

    test

--- src/main/java/com/ddobang/backend/domain/theme/entity/Theme.java
+++ src/main/java/com/ddobang/backend/domain/theme/entity/Theme.java
@@ -1,11 +1,12 @@
 package com.ddobang.backend.domain.theme.entity;
 
 import com.ddobang.backend.global.entity.BaseTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
 public class Theme extends BaseTime {
-    @Id
-    Long id;
+	@Id
+	Long idi;
 }
