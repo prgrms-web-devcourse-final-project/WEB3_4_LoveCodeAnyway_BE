e9ec3031576b3c750eb9564cf26d4c2b599b9a86
100gammmit <100minha@naver.com>
2025-03-26 15:20:10 +0900

    SuccessResponse 빌더 생성자를 사용해 팩토리 메서드 구성

--- src/main/java/com/ddobang/backend/global/response/SuccessResponse.java
+++ src/main/java/com/ddobang/backend/global/response/SuccessResponse.java
@@ -1,11 +1,27 @@
 package com.ddobang.backend.global.response;
 
-public record SuccessResponse<T>(String message, T data) {
-    public static <T> SuccessResponse<T> of(String message, T data) {
-        return new SuccessResponse<>(message, data);
-    }
+import lombok.AccessLevel;
+import lombok.Builder;
 
-    public static <T> SuccessResponse<T> of(String message) {
-        return new SuccessResponse<>(message, null);
-    }
+@Builder(access = AccessLevel.PRIVATE)
+public record SuccessResponse<T>(String message, T data) {
+
+	public static <T> SuccessResponse<T> of(String message) {
+		return SuccessResponse.<T>builder()
+			.message(message)
+			.build();
+	}
+
+	public static <T> SuccessResponse<T> of(T data) {
+		return SuccessResponse.<T>builder()
+			.data(data)
+			.build();
+	}
+
+	public static <T> SuccessResponse<T> of(String message, T data) {
+		return SuccessResponse.<T>builder()
+			.message(message)
+			.data(data)
+			.build();
+	}
 }
