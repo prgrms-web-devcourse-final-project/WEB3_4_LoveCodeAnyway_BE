c18a994facb7bb7df6d608cd55b14bb1b14627ec
100gammmit <100minha@naver.com>
2025-03-27 16:47:31 +0900

    feat: StoreService 관리자 전용 매장 정보 저장 메서드 작성

--- src/main/java/com/ddobang/backend/domain/store/service/StoreService.java
+++ src/main/java/com/ddobang/backend/domain/store/service/StoreService.java
@@ -1,11 +1,25 @@
 package com.ddobang.backend.domain.store.service;
 
-import com.ddobang.backend.domain.store.repository.StoreRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import com.ddobang.backend.domain.region.entity.Region;
+import com.ddobang.backend.domain.region.service.RegionService;
+import com.ddobang.backend.domain.store.dto.StoreRequest;
+import com.ddobang.backend.domain.store.entity.Store;
+import com.ddobang.backend.domain.store.repository.StoreRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class StoreService {
-    private final StoreRepository storeRepository;
+
+	private final StoreRepository storeRepository;
+
+	private final RegionService regionService;
+
+	public void saveForAdmin(StoreRequest storeRequest) {
+		Region region = regionService.findById(storeRequest.regionId());
+		storeRepository.save(Store.of(storeRequest, region));
+	}
 }
