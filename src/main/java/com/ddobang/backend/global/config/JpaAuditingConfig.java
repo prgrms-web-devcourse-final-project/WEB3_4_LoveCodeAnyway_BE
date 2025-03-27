5e61aed23919453c7fbb1824fe81bc79f827eb7e
100gammmit <100minha@naver.com>
2025-03-27 11:54:53 +0900

    feat: JpaAuditing 설정 클래스 분리
    - main함수 클래스에서 설정 시 단위 테스트에서 설정을 로드하지 못하는 현상으로 인해 분리함

---
+++ src/main/java/com/ddobang/backend/global/config/JpaAuditingConfig.java
@@ -0,0 +1,13 @@
+package com.ddobang.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * JpaAuditingConfig
+ * @author 100minha
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
