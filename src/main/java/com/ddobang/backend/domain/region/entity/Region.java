3ac068025e9acc437be92b895ea000ea4c45a072
100gammmit <100minha@naver.com>
2025-03-27 12:10:45 +0900

    feat: 테스트에서 사용할 builder 생성자 추가

--- src/main/java/com/ddobang/backend/domain/region/entity/Region.java
+++ src/main/java/com/ddobang/backend/domain/region/entity/Region.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +26,10 @@ public class Region {
 	String majorRegion;
 
 	String subRegion;
+
+	@Builder
+	public Region(String majorRegion, String subRegion) {
+		this.majorRegion = majorRegion;
+		this.subRegion = subRegion;
+	}
 }
