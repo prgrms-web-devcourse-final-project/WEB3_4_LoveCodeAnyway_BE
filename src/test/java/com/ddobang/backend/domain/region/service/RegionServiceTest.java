20542d48a650efeeb50305edac8dd7e7e8727f38
100gammmit <100minha@naver.com>
2025-03-27 15:03:34 +0900

    test: Region의 findById 관련 테스트 작성

--- src/test/java/com/ddobang/backend/domain/region/service/RegionServiceTest.java
+++ src/test/java/com/ddobang/backend/domain/region/service/RegionServiceTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 
 import com.ddobang.backend.domain.region.dto.SubRegionsResponse;
 import com.ddobang.backend.domain.region.entity.Region;
