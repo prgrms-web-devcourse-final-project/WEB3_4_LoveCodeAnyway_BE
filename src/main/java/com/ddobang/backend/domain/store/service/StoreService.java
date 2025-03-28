fb77e649f352371fbb2c9806266808b7492d8d2e
100gammmit <100minha@naver.com>
2025-03-28 17:13:32 +0900

    feat: 관리자 전용 수정 및 삭제 api에 사용할 메서드 작성

--- src/main/java/com/ddobang/backend/domain/store/service/StoreService.java
+++ src/main/java/com/ddobang/backend/domain/store/service/StoreService.java
@@ -1,11 +1,14 @@
 package com.ddobang.backend.domain.store.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ddobang.backend.domain.region.entity.Region;
 import com.ddobang.backend.domain.region.service.RegionService;
 import com.ddobang.backend.domain.store.dto.StoreRequest;
 import com.ddobang.backend.domain.store.entity.Store;
+import com.ddobang.backend.domain.store.exception.StoreErrorCode;
+import com.ddobang.backend.domain.store.exception.StoreException;
 import com.ddobang.backend.domain.store.repository.StoreRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,8 +21,33 @@ public class StoreService {
 
 	private final RegionService regionService;
 
+	@Transactional
 	public void saveForAdmin(StoreRequest storeRequest) {
 		Region region = regionService.findById(storeRequest.regionId());
 		storeRepository.save(Store.of(storeRequest, region));
+	}
+
+	@Transactional(readOnly = true)
+	public Store findById(Long id) {
+		return storeRepository.findById(id)
+			.orElseThrow(() -> new StoreException(StoreErrorCode.STORE_NOT_FOUND));
+	}
+
+	@Transactional
+	public void modify(Long id, StoreRequest storeRequest) {
+		Store store = findById(id);
+
+		Region region = regionService.findById(storeRequest.regionId());
+		store.modify(storeRequest, region);
+	}
+
+	/**
+	 * 매장 삭제 처리 메서드
+	 * Store.Status 를 DELETED 로 변경
+	 * @param id 삭제 대상 매장 id
+	 */
+	@Transactional
+	public void delete(Long id) {
+		findById(id).delete();
 	}
 }
