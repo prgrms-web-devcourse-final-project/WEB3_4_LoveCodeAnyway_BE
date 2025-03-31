685e43e5e9bf416dcf8cf2cfd32a315940f01b01
notmybussiness <cuisine97@naver.com>
2025-03-31 14:11:44 +0900

    feat: MessageRequestDto 작성

--- src/main/java/com/ddobang/backend/domain/message/dto/MessageRequestDto.java
+++ src/main/java/com/ddobang/backend/domain/message/dto/MessageRequestDto.java
@@ -2,7 +2,9 @@ package com.ddobang.backend.domain.message.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
+@Getter
 public class MessageRequestDto {
 
 	@NotNull(message = "수신자 ID는 필수입니다.")
