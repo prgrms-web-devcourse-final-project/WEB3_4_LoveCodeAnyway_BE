ebd78f1dbbf53a8f7579cfdfd609ca8df9531e53
100gammmit <100minha@naver.com>
2025-03-31 16:15:32 +0900

    feat: 테마 상세 조회 api 구현

--- src/main/java/com/ddobang/backend/domain/theme/controller/ThemeController.java
+++ src/main/java/com/ddobang/backend/domain/theme/controller/ThemeController.java
@@ -1,13 +1,29 @@
 package com.ddobang.backend.domain.theme.controller;
 
-import com.ddobang.backend.domain.theme.service.ThemeService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ddobang.backend.domain.theme.dto.ThemeDetailResponse;
+import com.ddobang.backend.domain.theme.service.ThemeService;
+import com.ddobang.backend.global.response.ResponseFactory;
+import com.ddobang.backend.global.response.SuccessResponse;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/themes")
 public class ThemeController {
-    private final ThemeService themeService;
+	private final ThemeService themeService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<SuccessResponse<ThemeDetailResponse>> getTheme(@PathVariable Long id) {
+		ThemeDetailResponse themeDetailResponse = themeService.getTheme(id);
+
+		return ResponseFactory.ok(themeDetailResponse);
+	}
+
 }
