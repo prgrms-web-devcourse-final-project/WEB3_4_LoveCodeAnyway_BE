7c21bc994886e744e89fc31b69edf2fb9a2387ab
notmybussiness <cuisine97@naver.com>
2025-03-31 11:34:49 +0900

    feat: 메세지 레포지토리 구현

--- src/main/java/com/ddobang/backend/domain/message/repository/MessageRepository.java
+++ src/main/java/com/ddobang/backend/domain/message/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.ddobang.backend.domain.message.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,16 @@ import com.ddobang.backend.domain.message.entity.Message;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+	// 수/발신자 메시지 조회
 	List<Message> findAllByReceiverId(Long receiverId);
 
+	List<Message> findAllBySenderId(Long senderId);
+
+	// 읽지않은 수신 메세지 조회
+	List<Message> findAllByReceiverIdAndIsReadFalse(Long receiverId);
+
+	// 수/발신자 메시지 페이징처리(최신순서로)
+	Page<Message> findAllByReceiverIdOrderByCreatedAtDesc(Long receiverId, Pageable pageable);
+
+	Page<Message> findAllBySenderIdOrderByCreatedAtDesc(Long senderId, Pageable pageable);
 }
