fc25969fddf15cf3315f46aed1f6dcbf27d8092a
universechoi <skye.of.universe@gmail.com>
2025-04-01 22:01:49 +0900

    feat: CustomRepository 함수 추가 및 Repository에 적용

--- src/main/java/com/ddobang/backend/domain/party/repository/PartyRepository.java
+++ src/main/java/com/ddobang/backend/domain/party/repository/PartyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PartyRepository extends JpaRepository<Party, Long> {
+public interface PartyRepository extends JpaRepository<Party, Long>, PartyRepositoryCustom {
 }
