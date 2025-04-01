530fb2621e10cb4480aaec82b93c35ad0f646981
Jay Lim <carneo.dev@gmail.com>
2025-04-01 09:22:29 +0900

    feat: OAuth2SuccessHandler에서 카카오 ID 추출 및 회원 조회 - OAuth2 의존성 추가

--- src/main/java/com/ddobang/backend/global/security/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/ddobang/backend/global/security/oauth/OAuth2SuccessHandler.java
@@ -1,10 +1,15 @@
 package com.ddobang.backend.global.security.oauth;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.ddobang.backend.domain.member.entity.Member;
+import com.ddobang.backend.domain.member.repository.MemberRepository;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,6 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+	private final MemberRepository memberRepository;
+
 	@Override
 	public void onAuthenticationSuccess(
 		HttpServletRequest request,
@@ -23,5 +30,21 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 		Authentication authentication
 	) throws IOException, ServletException { // OAuth2 로그인 성공 시 처리될 내용
 		log.info("OAuth2 로그인 성공: {}", authentication.getName());
+
+		// 사용자 정보 추출 (카카오 ID 기준)
+		OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
+		String kakaoId = String.valueOf(oAuth2User.getAttribute("id")); // or "sub"
+
+		log.info("카카오 ID: {}", kakaoId);
+
+		// 기존 회원 여부 확인
+		Optional<Member> existMember = memberRepository.findByOauthId(kakaoId);
+
+		if (existMember.isPresent()) {
+			log.info("기존 회원: {}", kakaoId);
+		} else {
+			log.info("신규 회원입니다: {}", kakaoId);
+			// 신규회원 가입 처리 로직
+		}
 	}
 }
