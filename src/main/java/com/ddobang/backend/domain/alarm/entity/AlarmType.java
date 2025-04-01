98eb95248f36f0ebf976ded5857f0c65efb3b97f
notmybussiness <cuisine97@naver.com>
2025-04-01 14:25:53 +0900

    fix: AlarmType Enum 변경

--- src/main/java/com/ddobang/backend/domain/alarm/entity/AlarmType.java
+++ src/main/java/com/ddobang/backend/domain/alarm/entity/AlarmType.java
@@ -1,9 +1,8 @@
 package com.ddobang.backend.domain.alarm.entity;
 
 public enum AlarmType {
-	SYSTEM,       // 시스템 알림
-	COMMENT,      // 댓글 알림
-	MENTION,      // 언급 알림
-	FOLLOW,       // 팔로우 알림
-	EVENT         // 이벤트 알림
+	SYSTEM,       // 시스템 알림 - 관리자 알림 전부
+	MESSAGE,      // 쪽지 알림
+	FOLLOW,       // 구독한 테마 모임 알림
+	OTHER         // 기타 알림
 }
