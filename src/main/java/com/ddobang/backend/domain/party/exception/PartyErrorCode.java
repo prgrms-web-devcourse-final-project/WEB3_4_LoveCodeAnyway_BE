059ef6a1f74e336825c7045e5aa74ac6797c0f5c
universechoi <skye.of.universe@gmail.com>
2025-04-01 21:57:03 +0900

    refactor: PartyErrorCode 정리 및 커스텀 메시지 생성자 제거

--- src/main/java/com/ddobang/backend/domain/party/exception/PartyErrorCode.java
+++ src/main/java/com/ddobang/backend/domain/party/exception/PartyErrorCode.java
@@ -11,16 +11,17 @@ public enum PartyErrorCode implements ErrorCode {
 	PARTY_HOST_NOT_FOUND(HttpStatus.UNPROCESSABLE_ENTITY, "PARTY_003", "모임에 모임장이 존재하지 않습니다."),
 	PARTY_INVALID_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "PARTY_004", "현재 상태에서는 모임 상태를 변경할 수 없습니다."),
 	PARTY_INVALID_PARTICIPANTS(HttpStatus.BAD_REQUEST, "PARTY_005", "모집 인원은 총 인원보다 많을 수 없습니다."),
-	PARTY_FULL(HttpStatus.BAD_REQUEST, "PARTY_006", "모임 인원이 이미 가득 찼습니다."),
+	PARTY_NOT_REQUITING(HttpStatus.BAD_REQUEST, "PARTY_006", "현재 모집 중인 파티가 아닙니다."),
+	PARTY_NOT_OPEN(HttpStatus.BAD_REQUEST, "PARTY_007", "모집 기간이 종료되어 상태를 변경할 수 없습니다."),
+	PARTY_NOT_EXECUTABLE(HttpStatus.BAD_REQUEST, "PARTY_008", "모임을 완료 또는 취소 처리할 수 있는 상태가 아닙니다."),
 
 	// Party Member
-
 	PARTY_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "PARTY_MEMBER_001", "모임원이 아닙니다."),
-	PARTY_MEMBER_NOT_HOST(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_002", "이 모임의 모임장이 아닙니다."),
-	PARTY_MEMBER_NOT_APPLICANT(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_003", "이 모임의 신청자가 아닙니다."),
-	PARTY_MEMBER_NOT_ACCEPTED(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_004", "이 모임의 승인된 모임원이 아닙니다."),
+	PARTY_MEMBER_NOT_HOST(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_002", "모임장이 아닙니다."),
+	PARTY_MEMBER_NOT_APPLICANT(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_003", "모임장이거나 이미 승인된 모임원입니다."),
 	PARTY_MEMBER_ALREADY_APPLIED(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_005", "이미 신청한 모임입니다."),
-	PARTY_MEMBER_ALREADY_ACCEPTED(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_006", "이미 승인된 모임원입니다.");
+	PARTY_MEMBER_ALREADY_ACCEPTED(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_006", "이미 승인된 모임입니다."),
+	PARTY_MEMBER_CANNOT_CANCEL_HOST(HttpStatus.BAD_REQUEST, "PARTY_MEMBER_007", "모임장은 참여를 취소할 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String errorCode;
