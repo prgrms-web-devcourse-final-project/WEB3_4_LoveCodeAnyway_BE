512efec79224e2b4ce8e795c1947217eca76ce83
solji <solji.choi89@gmail.com>
2025-04-02 15:19:32 +0900

    refactor: 탈출일지 initData 10개 생성

--- src/main/java/com/ddobang/backend/domain/diary/initdata/DiaryInitData.java
+++ src/main/java/com/ddobang/backend/domain/diary/initdata/DiaryInitData.java
@@ -13,6 +13,7 @@ import com.ddobang.backend.domain.diary.dto.request.DiaryRequestDto;
 import com.ddobang.backend.domain.diary.service.DiaryService;
 import com.ddobang.backend.domain.member.entity.Member;
 import com.ddobang.backend.domain.member.repository.MemberRepository;
+import com.ddobang.backend.domain.theme.initData.InitThemeMockData;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class DiaryInitData {
 	private final DiaryService diaryService;
 	private final MemberRepository memberRepository;
+	private final InitThemeMockData initThemeMockData;
 
 	@Autowired
 	@Lazy
@@ -29,6 +31,7 @@ public class DiaryInitData {
 	@Bean
 	public ApplicationRunner diaryInitDataApplicationRunner() {
 		return args -> {
+			initThemeMockData.initMockData();
 			self.memberInitData();
 			self.initData();
 		};
@@ -54,27 +57,29 @@ public class DiaryInitData {
 			return;
 		}
 
-		DiaryRequestDto diaryRequestDto = DiaryRequestDto.builder()
-			.themeId(1L)
-			.imageUrl("https://placehold.co/640x640?text=:P")
-			.escapeDate(LocalDate.of(2025, 2, 20))
-			.participants("지인1, 지인2")
-			.difficulty(3)
-			.fear(3)
-			.activity(3)
-			.satisfaction(3)
-			.production(3)
-			.story(3)
-			.question(3)
-			.interior(3)
-			.deviceRatio(70)
-			.hintCount(1)
-			.escapeResult(true)
-			.timeType("remaining")
-			.elapsedTime("15:25")
-			.review("너무 재밌었다!!")
-			.build();
-
-		diaryService.write(diaryRequestDto);
+		for (int i = 1; i <= 10; i++) {
+			diaryService.write(
+				DiaryRequestDto.builder()
+					.themeId((long)i)
+					.imageUrl("https://placehold.co/640x640?text=:P")
+					.escapeDate(LocalDate.of(2024, i, 15))
+					.participants("지인1, 지인2")
+					.difficulty(3)
+					.fear(3)
+					.activity(3)
+					.satisfaction(3)
+					.production(3)
+					.story(3)
+					.question(3)
+					.interior(3)
+					.deviceRatio(70)
+					.hintCount(i % 3)
+					.escapeResult(i % 2 == 0 ? true : false)
+					.timeType("remaining")
+					.elapsedTime("15:25")
+					.review("너무 재밌었다!!")
+					.build()
+			);
+		}
 	}
 }
