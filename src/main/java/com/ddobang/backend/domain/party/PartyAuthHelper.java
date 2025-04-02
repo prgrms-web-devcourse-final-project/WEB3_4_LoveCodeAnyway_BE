0738591aff599015eb9e98f782df00d6dcba6874
universechoi <skye.of.universe@gmail.com>
2025-04-02 14:36:41 +0900

    chore: PartyAuthHelper 삭제 예정에 대한 TODO 주석 추가

--- src/main/java/com/ddobang/backend/domain/party/PartyAuthHelper.java
+++ src/main/java/com/ddobang/backend/domain/party/PartyAuthHelper.java
@@ -9,6 +9,7 @@ import com.ddobang.backend.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
+// TODO: 임시 인증 헬퍼 클래스입니다. 멤버 도메인 쪽 인증 처리 완성되면 삭제 예정입니다
 @Component
 @RequiredArgsConstructor
 public class PartyAuthHelper {
