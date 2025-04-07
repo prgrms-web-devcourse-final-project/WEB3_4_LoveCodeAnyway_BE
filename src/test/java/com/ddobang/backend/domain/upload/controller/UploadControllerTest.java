70d381933b724710cdac7ecc3cff2b67a0f4072b
solji <solji.choi89@gmail.com>
2025-04-07 12:23:44 +0900

    fix: 업로드 테스트 케이스 충돌 문제 해결

--- src/test/java/com/ddobang/backend/domain/upload/controller/UploadControllerTest.java
+++ src/test/java/com/ddobang/backend/domain/upload/controller/UploadControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -43,6 +44,7 @@ import jakarta.persistence.EntityManager;
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @Transactional
+@DirtiesContext
 public class UploadControllerTest {
 	@Autowired
 	private MockMvc mvc;
