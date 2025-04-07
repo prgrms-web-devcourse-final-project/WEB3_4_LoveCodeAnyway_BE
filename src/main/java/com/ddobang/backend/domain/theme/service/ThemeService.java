be8e280e06a0282c1f0f4c9aa24e6d90d17643ac
100gammmit <100minha@naver.com>
2025-04-07 15:18:00 +0900

    feat: 관리자용 테마 관리 서비스 로직 작성

--- src/main/java/com/ddobang/backend/domain/theme/service/ThemeService.java
+++ src/main/java/com/ddobang/backend/domain/theme/service/ThemeService.java
@@ -8,13 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ddobang.backend.domain.store.entity.Store;
 import com.ddobang.backend.domain.store.service.StoreService;
-import com.ddobang.backend.domain.theme.dto.ThemeDetailResponse;
-import com.ddobang.backend.domain.theme.dto.ThemeFilterRequest;
-import com.ddobang.backend.domain.theme.dto.ThemeForDiaryResponse;
-import com.ddobang.backend.domain.theme.dto.ThemeForMemberRequest;
-import com.ddobang.backend.domain.theme.dto.ThemeForPartyResponse;
 import com.ddobang.backend.domain.theme.dto.ThemeStatDto;
-import com.ddobang.backend.domain.theme.dto.ThemesResponse;
+import com.ddobang.backend.domain.theme.dto.request.ThemeFilterRequest;
+import com.ddobang.backend.domain.theme.dto.request.ThemeForAdminRequest;
+import com.ddobang.backend.domain.theme.dto.request.ThemeForMemberRequest;
+import com.ddobang.backend.domain.theme.dto.response.SimpleThemeResponse;
+import com.ddobang.backend.domain.theme.dto.response.ThemeDetailResponse;
+import com.ddobang.backend.domain.theme.dto.response.ThemeForAdminResponse;
+import com.ddobang.backend.domain.theme.dto.response.ThemeForPartyResponse;
+import com.ddobang.backend.domain.theme.dto.response.ThemesResponse;
 import com.ddobang.backend.domain.theme.entity.Theme;
 import com.ddobang.backend.domain.theme.entity.ThemeTag;
 import com.ddobang.backend.domain.theme.exception.ThemeErrorCode;
@@ -40,22 +42,17 @@ public class ThemeService {
 
 		List<Theme> themes = themeRepository.findThemesByFilter(filterRequest, page, size);
 
-		return SliceDto.of(themes.stream()
-			.map(ThemesResponse::of)
-			.toList(), size);
+		return SliceDto.of(themes.stream().map(ThemesResponse::of).toList(), size);
 	}
 
 	@Transactional(readOnly = true)
 	public ThemeDetailResponse getThemeWithStat(Long id) {
 
-		Theme theme = themeRepository.findById(id).orElseThrow(
-			() -> new ThemeException(ThemeErrorCode.THEME_NOT_FOUND)
-		);
+		Theme theme = themeRepository.findById(id)
+			.orElseThrow(() -> new ThemeException(ThemeErrorCode.THEME_NOT_FOUND));
 
 		// 테마 통계가 등록되어있다면 통계 반영, 없다면 통계 부분은 null 반환
-		ThemeStatDto themeStatDto = themeStatRepository.findById(id)
-			.map(ThemeStatDto::of)
-			.orElse(null);
+		ThemeStatDto themeStatDto = themeStatRepository.findById(id).map(ThemeStatDto::of).orElse(null);
 
 		return ThemeDetailResponse.of(theme, themeStatDto);
 	}
@@ -65,31 +62,27 @@ public class ThemeService {
 
 		List<Theme> themes = themeRepository.findThemesForPartySearch(keyword);
 
-		return themes.stream().map(ThemeForPartyResponse::of)
-			.collect(Collectors.toList());
+		return themes.stream().map(ThemeForPartyResponse::of).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
-	public List<ThemeForDiaryResponse> getThemesForDiarySearch(String keyword) {
+	public List<SimpleThemeResponse> getThemesForDiarySearch(String keyword) {
 
 		return themeRepository.findThemesForDiarySearch(keyword);
 	}
 
 	public Theme getThemeById(Long id) {
-		return themeRepository.findById(id).orElseThrow(
-			() -> new ThemeException(ThemeErrorCode.THEME_NOT_FOUND));
+		return themeRepository.findById(id).orElseThrow(() -> new ThemeException(ThemeErrorCode.THEME_NOT_FOUND));
 	}
 
 	/**
 	 * 사용자 일지 등록 전용 테마 저장 메서드
 	 * @param request ThemeForMemberRequest
 	 */
-	public ThemeForDiaryResponse saveThemeForMember(ThemeForMemberRequest request) {
-		List<ThemeTag> themeTags = request.tags().stream()
-			.map(themeTagService::getByName)
-			.toList();
-		Store store = storeService.saveForMember(Store.builder()
-			.name(request.storeName()).build());
+	@Transactional
+	public SimpleThemeResponse saveForMember(ThemeForMemberRequest request) {
+		List<ThemeTag> themeTags = request.tags().stream().map(themeTagService::getByName).toList();
+		Store store = storeService.saveForMember(Store.builder().name(request.storeName()).build());
 
 		Theme savedTheme = themeRepository.save(Theme.builder()
 			.name(request.themeName())
@@ -99,6 +92,44 @@ public class ThemeService {
 			.themeTags(themeTags)
 			.build());
 
-		return ThemeForDiaryResponse.of(savedTheme);
+		return SimpleThemeResponse.of(savedTheme);
+	}
+
+	@Transactional
+	public void saveForAdmin(ThemeForAdminRequest request) {
+		List<ThemeTag> themeTags = request.tags().stream().map(themeTagService::getByName).toList();
+		Store store = storeService.findById(request.storeId());
+
+		themeRepository.save(Theme.of(request, store, themeTags));
+	}
+
+	@Transactional
+	public void modify(Long id, ThemeForAdminRequest request) {
+		Theme theme = getThemeById(id);
+		List<ThemeTag> themeTags = request.tags().stream().map(themeTagService::getByName).toList();
+		Store store = storeService.findById(request.storeId());
+
+		theme.modify(request, store, themeTags);
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		Theme theme = getThemeById(id);
+
+		theme.delete();
+	}
+
+	@Transactional(readOnly = true)
+	public ThemeForAdminResponse getThemeForAdmin(Long id) {
+		Theme theme = getThemeById(id);
+
+		return ThemeForAdminResponse.of(theme);
+	}
+
+	@Transactional(readOnly = true)
+	public SliceDto<SimpleThemeResponse> getThemesWithFilterForAdmin(ThemeFilterRequest filterRequest, int page,
+		int size) {
+		List<SimpleThemeResponse> themes = themeRepository.findThemesForAdminSearch(filterRequest, page, size);
+		return SliceDto.of(themes, size);
 	}
 }
