492d7debc73e4f2d76056911e93f7bade589304c
100gammmit <100minha@naver.com>
2025-04-07 11:07:11 +0900

    feat: 관리자 전용 매장 검색 로직 작성

--- src/main/java/com/ddobang/backend/domain/store/service/StoreService.java
+++ src/main/java/com/ddobang/backend/domain/store/service/StoreService.java
@@ -1,11 +1,14 @@
 package com.ddobang.backend.domain.store.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ddobang.backend.domain.region.entity.Region;
 import com.ddobang.backend.domain.region.service.RegionService;
 import com.ddobang.backend.domain.store.dto.StoreRequest;
+import com.ddobang.backend.domain.store.dto.StoreResponse;
 import com.ddobang.backend.domain.store.entity.Store;
 import com.ddobang.backend.domain.store.exception.StoreErrorCode;
 import com.ddobang.backend.domain.store.exception.StoreException;
@@ -54,5 +57,10 @@ public class StoreService {
 	@Transactional
 	public Store saveForMember(Store store) {
 		return storeRepository.save(store);
+	}
+
+	@Transactional(readOnly = true)
+	public List<StoreResponse> getStoresByKeyword(String keyword) {
+		return storeRepository.findStoresByKeyword(keyword);
 	}
 }
