7848e1cdb0d90c1e94c6b281866a0a45934909ee
notmybussiness <cuisine97@naver.com>
2025-04-09 20:23:45 +0900

    fix

--- src/main/java/com/ddobang/backend/domain/diary/repository/DiaryStatRepository.java
+++ src/main/java/com/ddobang/backend/domain/diary/repository/DiaryStatRepository.java
@@ -9,5 +9,8 @@ import com.ddobang.backend.domain.diary.entity.DiaryStat;
 
 @Repository
 public interface DiaryStatRepository extends JpaRepository<DiaryStat, Long> {
+<<<<<<< HEAD
 	List<DiaryStat> findByThemeId(Long themeId);
+=======
+>>>>>>> 85c27b2 (gemini review plz)
 }
