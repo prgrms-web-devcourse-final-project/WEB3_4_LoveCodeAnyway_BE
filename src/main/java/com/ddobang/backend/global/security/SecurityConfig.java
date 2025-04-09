9e2bb4d384316b685545f10e42244cda826a7a05
Jay Lim <carneo.dev@gmail.com>
2025-04-09 09:40:37 +0900

    fix: PR 피드백 반영 - 가독성을 위해 CorsConfigrationSource 직접 반영

--- src/main/java/com/ddobang/backend/global/security/SecurityConfig.java
+++ src/main/java/com/ddobang/backend/global/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,7 +36,7 @@ public class SecurityConfig {
 		HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
 
 		http
-			.cors(Customizer.withDefaults())
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(session
 				-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
