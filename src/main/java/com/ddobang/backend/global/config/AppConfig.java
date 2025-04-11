36a3a3d47f51dab0a17c9b3f1afea4c0515eac15
Jay Lim <carneo.dev@gmail.com>
2025-04-12 02:41:53 +0900

    feat: 회원 통계 관련 베이스 파일 추가 from @solji

--- src/main/java/com/ddobang/backend/global/config/AppConfig.java
+++ src/main/java/com/ddobang/backend/global/config/AppConfig.java
@@ -1,7 +1,19 @@
 package com.ddobang.backend.global.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Getter;
 
 @Configuration
 public class AppConfig {
+	@Getter
+	private static ObjectMapper objectMapper;
+
+	@Autowired
+	public void setObjectMapper(ObjectMapper objectMapper) {
+		AppConfig.objectMapper = objectMapper;
+	}
 }
