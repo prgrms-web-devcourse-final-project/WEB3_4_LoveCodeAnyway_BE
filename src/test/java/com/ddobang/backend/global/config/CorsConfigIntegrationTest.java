c4a473f56c63c8434548c7c182e8c4401f04a6e2
Jay Lim <carneo.dev@gmail.com>
2025-04-11 10:28:46 +0900

    fix: CorsConfigIntegrationTest @Profile("test") 추가

--- src/test/java/com/ddobang/backend/global/config/CorsConfigIntegrationTest.java
+++ src/test/java/com/ddobang/backend/global/config/CorsConfigIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@Profile("test")
 class CorsConfigIntegrationTest {
 
 	@Autowired
