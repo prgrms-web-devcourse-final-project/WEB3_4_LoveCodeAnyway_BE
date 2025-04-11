7a3266319630fccc0152e0a9f42480f3f8c58845
Jay Lim <carneo.dev@gmail.com>
2025-04-12 04:41:15 +0900

    fix: JWT 토큰 구조 변경

--- src/main/java/com/ddobang/backend/global/security/jwt/JwtTokenFactory.java
+++ src/main/java/com/ddobang/backend/global/security/jwt/JwtTokenFactory.java
@@ -4,6 +4,11 @@ import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import com.ddobang.backend.domain.member.entity.Member;
+import com.ddobang.backend.domain.member.exception.MemberErrorCode;
+import com.ddobang.backend.domain.member.exception.MemberException;
+import com.ddobang.backend.domain.member.service.MemberService;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
@@ -17,17 +22,22 @@ public class JwtTokenFactory {
 
 	private final JwtTokenProperties jwtTokenProperties;
 	private final JwtSigningKey jwtSigningKey;
+	private final MemberService memberService;
 
 	// JWT 토큰을 생성하는 메서드
-	public String generateToken(String subject, JwtTokenType type, boolean isAdmin) {
+	public String generateToken(String kakaoId, JwtTokenType type, boolean isAdmin) {
+		Member member = memberService.findByKakaoId(kakaoId)
+			.orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
 		long expiration = jwtTokenProperties.getExpiration(type);
 		Date now = new Date();
 		Date expiry = new Date(now.getTime() + expiration);
 
 		return Jwts.builder()
-			.setSubject(subject)
+			.setSubject(String.valueOf(member.getId()))
 			.setIssuedAt(now)
 			.setExpiration(expiry)
+			.claim("nickname", member.getNickname())
 			.claim("type", type.name())
 			.claim("isAdmin", isAdmin)
 			.signWith(jwtSigningKey.getKey(), SignatureAlgorithm.HS256)
