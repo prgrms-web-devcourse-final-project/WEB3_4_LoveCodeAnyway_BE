ea5b199fb78cf68c071119ecf6db80a8e02600ec
100gammmit <98521540+100gammmit@users.noreply.github.com>
2025-04-12 01:24:37 +0900

    Update PartyRepositoryTest.java import 추가

--- src/test/java/com/ddobang/backend/domain/party/repository/PartyRepositoryTest.java
+++ src/test/java/com/ddobang/backend/domain/party/repository/PartyRepositoryTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
