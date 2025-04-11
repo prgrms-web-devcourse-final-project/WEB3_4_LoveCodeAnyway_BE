bcf6c7143e296ee4b8ed1c38b9d380b8b892169c
notmybussiness <cuisine97@naver.com>
2025-04-12 02:53:22 +0900

    chore: 액티브 프로필

--- src/test/java/com/ddobang/backend/global/config/CorsConfigIntegrationTest.java
+++ src/test/java/com/ddobang/backend/global/config/CorsConfigIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CorsConfigIntegrationTest {
 
 	@Autowired
