ba055023035740a695d6b71d7615b6babb515100
solji <solji.choi89@gmail.com>
2025-04-11 15:58:47 +0900

    feat: 난이도별 힌트 갯수, 만족도 점수 쿼리 dsl 구현

--- src/main/java/com/ddobang/backend/domain/diary/repository/DiaryStatRepositoryCustom.java
+++ src/main/java/com/ddobang/backend/domain/diary/repository/DiaryStatRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.ddobang.backend.domain.diary.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import com.querydsl.core.Tuple;
 
@@ -8,4 +9,8 @@ public interface DiaryStatRepositoryCustom {
 	List<Tuple> top5TagCountSuccessCountByMember(Long authorId);
 
 	Long countTotalGenreBaseByMember(Long authorId);
+
+	Map<Integer, Tuple> difficultyStatsWithHints(Long authorId);
+
+	Map<Integer, Tuple> difficultyStatsWithSatisfaction(Long authorId);
 }
